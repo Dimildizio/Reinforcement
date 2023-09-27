83f63ddde24044455311296b489880648ab0631c
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-09-28 02:46:17 +0800

    feat(taxi): add agent class for model to learn

--- taxi_game/taxi.py
+++ taxi_game/taxi.py
@@ -2,20 +2,39 @@ import gym
 import numpy as np
 import time
 
-
 EPISODE_LENGTH = 100
+LR = 0.1
 
 
 class Agent:
+    def __init__(self, n_states, n_actions):
+        self.n_states = n_states
+        self.n_action = n_actions
+        self.model = np.random.rand(n_states, n_actions)
+
     def select_action(self, state):
+        # return np.argmax(self.model[state])
         return np.random.randint(6)
+
+    def update_agent(self, elite_trajectories, lr=LR):
+        new_model = np.zeros_like(self.model)
+        for trajectory in elite_trajectories:
+            for state, action in zip(trajectory['states'], trajectory['actions']):
+                new_model[state][action] += 1
+
+        for state in range(self.n_states):
+            state_sum = np.sum(new_model[state])
+            if state_sum > 0:
+                new_model[state] /= state_sum
+        self.model = (1 - lr) * self.model + (lr * self.model)
 
 
 class Environment:
-    def __init__(self):
+    def __init__(self, visualize=True):
         self.env = gym.make('Taxi-v3')
         self.num_states = self.env.observation_space.n  # 500 = 25 pos * 5 passenger locations * 4 destinations
         self.num_actions = self.env.action_space.n     # 6 = N, S, W, E, drop, pick up
+        self.visualize = visualize
 
     def initial_state(self):
         return self.env.reset()
@@ -34,7 +53,7 @@ class Environment:
             states.append(next_state)
             actions.append(action)
             rewards.append(reward)
-
+            self.draw()
             if done:
                 break
             state = next_state
@@ -43,3 +62,7 @@ class Environment:
                       'actions': actions,
                       'rewards': rewards}
         return trajectory
+
+    def draw(self):
+        if self.visualize:
+            self.env.render()
